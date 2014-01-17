7d8136f1722a646e75e89352c8564c3421812142
Lynn <LynnConwaySciTech@gmail.com>
2014-01-16 18:31:34 -0800

    pulling example graph code

--- makeEquation/interface.py
+++ makeEquation/interface.py
@@ -1,3 +1,6 @@
+#Basic
+
+'''
 import tkinter as tk
 
 class GUI(tk.Frame):
@@ -13,3 +16,132 @@ class GUI(tk.Frame):
         
 run = GUI()
 run.mainloop()
+'''
+
+#Buttons
+
+'''
+import tkinter
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+class GUI ():
+    def __init__(self, master):
+        # Create a container
+        frame = tkinter.Frame(master)
+        # Create 2 buttons
+        self.button_left = tkinter.Button(frame,text="< Decrease Slope",command=self.decrease)
+        self.button_left.pack(side="left")
+        self.button_right = tkinter.Button(frame,text="Increase Slope >",command=self.increase)
+        self.button_right.pack(side="left")
+
+        fig = Figure()
+        ax = fig.add_subplot(111)
+        self.line, = ax.plot(range(10))
+
+        self.canvas = FigureCanvasTkAgg(fig,master=master)
+        self.canvas.show()
+        self.canvas.get_tk_widget().pack(side='top', fill='both', expand=1)
+        frame.pack()
+
+    def decrease(self):
+        x, y = self.line.get_data()
+        self.line.set_ydata(y - 0.2 * x)
+        self.canvas.draw()
+
+    def increase(self):
+        x, y = self.line.get_data()
+        self.line.set_ydata(y + 0.2 * x)
+        self.canvas.draw()
+
+root = tkinter.Tk()
+run = GUI(root)
+root.mainloop()
+'''
+
+#Click event handler and annotation
+
+'''
+import math
+import pylab
+import matplotlib
+
+
+class AnnoteFinder:
+  """
+  callback for matplotlib to display an annotation when points are clicked on.  The
+  point which is closest to the click and within xtol and ytol is identified.
+    
+  Register this function like this:
+    
+  scatter(xdata, ydata)
+  af = AnnoteFinder(xdata, ydata, annotes)
+  connect('button_press_event', af)
+  """
+
+  def __init__(self, xdata, ydata, annotes, axis=None, xtol=None, ytol=None):
+    self.data = zip(xdata, ydata, annotes)
+    if xtol is None:
+      xtol = ((max(xdata) - min(xdata))/float(len(xdata)))/2
+    if ytol is None:
+      ytol = ((max(ydata) - min(ydata))/float(len(ydata)))/2
+    self.xtol = xtol
+    self.ytol = ytol
+    if axis is None:
+      self.axis = pylab.gca()
+    else:
+      self.axis= axis
+    self.drawnAnnotations = dict()
+    self.links = list()
+
+  def distance(self, x1, x2, y1, y2):
+    """
+    return the distance between two points
+    """
+    return math.hypot(x1 - x2, y1 - y2)
+
+  def __call__(self, event):
+    if event.inaxes:
+      clickX = event.xdata
+      clickY = event.ydata
+      if self.axis is None or self.axis==event.inaxes:
+        annotes = list()
+        for x,y,a in self.data:
+          if  clickX-self.xtol < x < clickX+self.xtol and  clickY-self.ytol < y < clickY+self.ytol :
+            annotes.append((self.distance(x,clickX,y,clickY),x,y, a) )
+        if annotes:
+          annotes.sort()
+          distance, x, y, annote = annotes[0]
+          self.drawAnnote(event.inaxes, x, y, annote)
+          for l in self.links:
+            l.drawSpecificAnnote(annote)
+
+  def drawAnnote(self, axis, x, y, annote):
+    """
+    Draw the annotation on the plot
+    """
+    if (x,y) in self.drawnAnnotations:
+      markers = self.drawnAnnotations[(x,y)]
+      for m in markers:
+        m.set_visible(not m.get_visible())
+      self.axis.figure.canvas.draw()
+    else:
+      t = axis.text(x,y, "(%3.2f, %3.2f) - %s"%(x,y,annote), )
+      m = axis.scatter([x],[y], marker='d', c='r', zorder=100)
+      self.drawnAnnotations[(x,y)] =(t,m)
+      self.axis.figure.canvas.draw()
+
+  def drawSpecificAnnote(self, annote):
+    annotesToDraw = [(x,y,a) for x,y,a in self.data if a==annote]
+    for x,y,a in annotesToDraw:
+      self.drawAnnote(self.axis, x, y, a)
+
+x = range(2)
+y = range(2)
+annotes = ["point 1","point 2"]
+
+pylab.scatter(x,y)
+af = AnnoteFinder(x, y, annotes)
+pylab.connect('button_press_event', af)
+pylab.show()
+'''
