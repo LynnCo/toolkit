d4141417a8b0211b841f359a4c0ab6df67aa73dc
lynnco <lynnconwayscitech@gmail.com>
2014-02-17 15:17:23 -0800

    command line args up
    also added an example gif

--- graphVis/visualize.py
+++ graphVis/visualize.py
@@ -21,35 +21,40 @@ import numpy
 import PIL
 import matplotlib
 import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import os
+import sys
 import images2gif
 
 def visualize(display):
-    matplotlib.pyplot.ion()
+    plt.ion()
     npframes = numpy.load("data/output.npy")
     xs = range(npframes[0].shape[0]+1)
     ys = range(npframes[0].shape[1]+1)
     X,Y = numpy.meshgrid(xs,ys)
-    dataList = matplotlib.pyplot.pcolormesh(X,Y,npframes[0],cmap="hot")
+    dataList = plt.pcolormesh(X,Y,npframes[0],cmap="hot")
     images = list()
     for i in range(1,len(npframes)):
-        thisFig = "img/figure"+str(i)+".png"
+        thisFig = "img/figure"+str(i)+".jpg"
         dataList.set_array(npframes[i].ravel())
         dataList.autoscale()
-        if display == "png":
-            matplotlib.pyplot.savefig(thisFig)
         if display == "gif":
-            matplotlib.pyplot.savefig(thisFig)
+            plt.savefig(thisFig)
             images.append(PIL.Image.open(thisFig))
             os.remove(thisFig)
-        if display == "plot":
-            matplotlib.pyplot.draw()
+        elif display == "png":
+            plt.savefig(thisFig)
+        elif display == "plot":
+            plt.draw()
             time.sleep(0.025)
+        else:
+            print("invalid display")
+            return "False"
     if display == "gif":
-        animName = "img/output_gif.gif"
+        animName = "img/plot_gif.gif"
         images2gif.writeGif(filename=animName, images=images, duration=0.1, repeat=True)
     print("visualization successful")
     return "True"
 
 if __name__ == "__main__":
-    visualize("png")
+    visualize(sys.argv[1]) 
